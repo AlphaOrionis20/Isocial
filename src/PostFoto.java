9e22d59e6cc333efdbabe476f49dd34d8390f3a4
Miguel Ângelo <angelomvieira20@gmail.com>
2023-05-31 15:43:15 -0300

    Criando classe PostFoto

---
+++ src/PostFoto.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class PostFoto implements Postavel
+{
+    private int qtd_de_fotos;
+    private LocalDateTime data_postagem;
+    private ArrayList<Foto> fotos = new ArrayList();
+    private String localizacao;
+
+    public PostFoto(){}
+    public boolean adicionaFoto()
+    {
+        return true;
+    }
+    @Override
+    public boolean posta() {
+        return false;
+    }
+
+    @Override
+    public boolean comenta() {
+        return false;
+    }
+}
